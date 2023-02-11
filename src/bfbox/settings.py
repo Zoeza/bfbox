c343d543a67233361ae046633cb2aed255ecdff7
Zoeza <zahrahabitaa@gmail.com>
2023-02-11 08:24:16 +0100

    Update settings.py

--- src/bfbox/settings.py
+++ src/bfbox/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'dashboard-root/site/public/static'
+STATIC_ROOT = 'bfbox/site/public/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'dashboard/site/public/media'
+MEDIA_ROOT = 'bfbox/site/public/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
