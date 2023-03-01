16cf83077e3f6b73b739b8186954fa4e6c91f1ec
Zoeza <zahrahabitaa@gmail.com>
2023-03-01 22:26:02 +0100

    mm

--- src/bfbox/settings.py
+++ src/bfbox/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard',
-    'phonenumber_field',
     'clients',
     'appointments',
     'reports',
--- src/clients/models.py
+++ src/clients/models.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.db import models
 from datetime import date
-from phonenumber_field.modelfields import PhoneNumberField
 
 
-class Client(models.Model):
+class Client(models.Model, ):
     objects = models.Manager()
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone = PhoneNumberField()
     address = models.CharField(max_length=100)
     birthday = models.DateField(default=date.today())
     lawyer = models.CharField(max_length=100)
