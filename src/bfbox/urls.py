84c1f3492502595e89a243726c0d6d7063c6b83b
Zoeza <zahrahabitaa@gmail.com>
2023-03-02 16:07:29 +0100

    up

--- src/bfbox/urls.py
+++ src/bfbox/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.static import static
 from django.urls import include, path
 from bfbox import settings
 
-urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('dashboard.urls')),
-                  path('clients/', include('clients.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [path('', include('dashboard.urls')),
+               path('clients/', include('clients.urls')),
+               path('admin/', admin.site.urls),
+
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/clients/views.py
+++ src/clients/views.py
@@ -1,27 +1,12 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import Client
-from django.contrib.auth.decorators import login_required
 
 
-@login_required
 def add_client(request):
     return render(request, "clients/add_client.html", {})
 
 
-@login_required
-def submit_client(request):
-    client = Client()
-    client.name = request.POST['name']
-    client.email = request.POST['email']
-    client.birthday = request.POST['birthday']
-    client.lawyer = request.POST['lawyer']
-    client.address = request.POST['address']
-    client.save()
-    return redirect('clients:manage-client')
-
-
-@login_required
 def manage_client(request):
     context = {
         'clients': Client.objects.all(),
@@ -29,7 +14,6 @@ def manage_client(request):
     return render(request, "clients/list_client.html", context)
 
 
-@login_required
 def edit_client(request, id):
     obj = Client.objects.get(id=id)
     context = {
@@ -43,14 +27,12 @@ def edit_client(request, id):
     return render(request, "clients/edit_client.html", context)
 
 
-@login_required
 def delete_client(request, id):
     obj = Client.objects.get(id=id)
     obj.delete()
     return redirect('clients:manage-client')
 
 
-@login_required
 def update_client(request, id):
     obj = Client(id=id)
     obj.name = request.POST['name']
