791cb046286c2fad4a52a1f1b37b754e02951930
Zoeza <zahrahabitaa@gmail.com>
2023-03-02 13:43:33 +0100

    Update urls.py

--- src/bfbox/urls.py
+++ src/bfbox/urls.py
@@ -6,6 +6,5 @@ from bfbox import settings
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('dashboard.urls')),
-                  path('clients/', include('clients.urls')),
                   path('appointments/', include('appointments.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
