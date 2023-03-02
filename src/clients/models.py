50ff4091f201239ab27824fc728c1cc04fab389d
Zoeza <zahrahabitaa@gmail.com>
2023-03-02 13:34:37 +0100

    Update models.py

--- src/clients/models.py
+++ src/clients/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from datetime import date
 
 
-class Client(models.Model, ):
+class Client(models.Model):
     objects = models.Manager()
     name = models.CharField(max_length=100)
     email = models.EmailField()
