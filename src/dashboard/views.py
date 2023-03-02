2876f6af3382b442acf059436592964fc52f0c2d
Zoeza <zahrahabitaa@gmail.com>
2023-03-02 16:16:53 +0100

    update

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html", {})
+    return render(request, "clients/list_client.html", {})
