feb9675dc5331bdd301764e3dfd12731e6e8f8c4
Zoeza <zahrahabitaa@gmail.com>
2023-03-02 13:12:26 +0100

    up

--- src/clients/urls.py
+++ src/clients/urls.py
@@ -7,9 +7,6 @@ urlpatterns = [
 
     path('add-client/', views.add_client, name='add-client'),
     path('manage-client/', views.manage_client, name='manage-client'),
-    path('edit-client/<int:id>', views.edit_client, name='edit-client'),
-    path('delete-client/<int:id>', views.delete_client, name='delete-client'),
-    path('update-client/<int:id>', views.update_client, name='update-client'),
 
 
 ]
