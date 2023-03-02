b9c8963ecc90659abfa6fb4e84866c8b28c0eb72
Zoeza <zahrahabitaa@gmail.com>
2023-03-02 16:45:06 +0100

    update

---
+++ src/clients/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'clients'
+
+urlpatterns = [
+
+    path('add-client/', views.add_client, name='add-client'),
+    path('manage-client/', views.manage_client, name='manage-client'),
+
+
+
+]
--- src/clients/views.py
+++ src/clients/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .models import Client
 
-# Create your views here.
+
+def add_client(request):
+    return render(request, "clients/add_client.html", {})
+
+
+def manage_client(request):
+    context = {
+        'clients': Client.objects.all(),
+    }
+    return render(request, "clients/list_client.html", context)
+
+
+def edit_client(request, id):
+    obj = Client.objects.get(id=id)
+    context = {
+        "name": obj.name,
+        "email": obj.email,
+        "phone": obj.phone,
+        "birthday": obj.birthday,
+        "lawyer": obj.lawyer,
+        "address": obj.address,
+        "id": obj.id
+    }
+    return render(request, "clients/edit_client.html", context)
+
+
+def delete_client(request, id):
+    obj = Client.objects.get(id=id)
+    obj.delete()
+    return redirect('clients:manage-client')
+
+
+def update_client(request, id):
+    obj = Client(id=id)
+    obj.name = request.POST['name']
+    obj.email = request.POST['email']
+    obj.phone = request.POST['phone']
+    obj.birthday = request.POST['birthday']
+    obj.lawyer = request.POST['lawyer']
+    obj.address = request.POST['address']
+    obj.save()
+    return redirect('clients:manage-client')
