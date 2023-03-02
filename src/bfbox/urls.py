4530f4c8879eeaf4de02152de122542787d6e0ac
Zoeza <zahrahabitaa@gmail.com>
2023-03-02 13:40:00 +0100

    update

--- src/bfbox/urls.py
+++ src/bfbox/urls.py
@@ -4,8 +4,8 @@ from django.urls import include, path
 from bfbox import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
-    path('clients/', include('clients.urls')),
-    path('appointments/', include('appointments.urls')),
+                  path('admin/', admin.site.urls),
+                  path('', include('dashboard.urls')),
+                  path('clients/', include('clients.urls')),
+                  path('appointments/', include('appointments.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
