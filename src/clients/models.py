44b3eff4188d6312fe499642c2a10a651d7e73c8
Zoeza <zahrahabitaa@gmail.com>
2023-03-04 00:22:13 +0100

    update

--- src/clients/models.py
+++ src/clients/models.py
@@ -7,7 +7,9 @@ class Client(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     address = models.CharField(max_length=100)
+    town = models.CharField(max_length=100)
     birthday = models.DateField(default=date.today())
+    created_date = models.DateField(default=date.today())
     lawyer = models.CharField(max_length=100)
 
     def __str__(self):
