3dc0dd25f3434452ba96dd9e3f12d3a8ccf65c7c
Zoeza <zahrahabitaa@gmail.com>
2023-03-03 16:50:20 +0100

    update

--- src/clients/views.py
+++ src/clients/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render, redirect
 from .models import Client
 
 
-
-
-
-
 def manage_client(request):
     context = {
         'clients': Client.objects.all(),
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def dashboard(request):
-    return render(request, "clients/add_client.html", {})
+    return render(request, "dashboard/dashboard.html", {})
