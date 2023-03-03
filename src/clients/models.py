0d8281122bb9557acbf24fb1dd1c64b62dcb83b4
Zoeza <zahrahabitaa@gmail.com>
2023-03-04 00:55:18 +0100

    update

--- src/clients/models.py
+++ src/clients/models.py
@@ -6,11 +6,11 @@ class Client(models.Model):
     objects = models.Manager()
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    birthday = models.DateField(default=date.today())
     address = models.CharField(max_length=100)
     town = models.CharField(max_length=100)
-    birthday = models.DateField(default=date.today())
-    created_date = models.DateField(default=date.today())
     lawyer = models.CharField(max_length=100)
+    created_date = models.DateField(default=date.today())
 
     def __str__(self):
         return self.name
--- src/clients/views.py
+++ src/clients/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render, redirect
 from .models import Client
 
 
+def add_client(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', False)
+        email = request.POST.get('email', False)
+        birthday = request.POST.get('birthday', False)
+        address = request.POST.get('address', False)
+        town = request.POST.get('town', False)
+        lawyer = request.POST.get('lawyer', False)
+        Client(name=name,
+               email=email,
+               birthday=birthday,
+               address=address,
+               town=town,
+               lawyer=lawyer,
+               ).save()
+    return redirect('clients:manage-client')
+
+
 def manage_client(request):
     context = {
         'clients': Client.objects.all(),
