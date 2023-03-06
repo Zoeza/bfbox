4bf6741b7d57d46f68550a74f8ac2fe111fee7aa
Zoeza <zahrahabitaa@gmail.com>
2023-03-06 23:55:20 +0100

    Update models.py

--- src/clients/models.py
+++ src/clients/models.py
@@ -11,8 +11,8 @@ class Client(models.Model):
     town = models.CharField(max_length=100)
     lawyer = models.CharField(max_length=100)
     sku = models.CharField(max_length=20, unique=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=False, null=True)
+    updated_at = models.DateTimeField(auto_now=False, null=True)
 
     def __str__(self):
         return self.name
