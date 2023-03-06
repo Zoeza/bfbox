42a4d5e66038897446cbb602a6f8b04784c1ad9b
Zoeza <zahrahabitaa@gmail.com>
2023-03-06 23:34:47 +0100

    update

--- src/clients/views.py
+++ src/clients/views.py
@@ -30,8 +30,8 @@ def add_client(request):
 
 # ------------------ update client ---------------------- #
 
-def update_client(request, sku):
-    selected_client = Client.objects.all().get(sku=sku)
+def update_client(request,id):
+    selected_client = Client.objects.all().get(id=id)
     if request.method == 'POST':
         name = request.POST.get('name', False)
         email = request.POST.get('email', False)
