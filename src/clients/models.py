5a999bad863a11b39e7f5277db08c2f8f9bdb81f
Zoeza <zahrahabitaa@gmail.com>
2023-03-06 15:51:25 +0100

    Update models.py

--- src/clients/models.py
+++ src/clients/models.py
@@ -13,7 +13,6 @@ class Client(models.Model):
     sku = models.CharField(max_length=20, unique=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
