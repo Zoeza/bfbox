8704ddb66c120efec312c2fb1fd613bc90795397
Zoeza <zahrahabitaa@gmail.com>
2023-03-06 23:50:53 +0100

    Update models.py

--- src/clients/models.py
+++ src/clients/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 
 
 class Client(models.Model):
@@ -11,8 +11,8 @@ class Client(models.Model):
     town = models.CharField(max_length=100)
     lawyer = models.CharField(max_length=100)
     sku = models.CharField(max_length=20, unique=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.name
