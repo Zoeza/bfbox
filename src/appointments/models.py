8d6bb6084de1d6dcd4aab183dfd1695f9ac1a9c5
Zoeza <zahrahabitaa@gmail.com>
2023-03-07 18:29:58 +0100

    update

--- src/appointments/models.py
+++ src/appointments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Appointment(models.Model):
+    objects = models.Manager()
+    title = models.CharField(max_length=255)
+    client = models.CharField(max_length=255)
+    time = models.DateTimeField(auto_now_add=False, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
