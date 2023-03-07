60bd0635e7feb455b0ab56ae7d3bbb352b3193ec
Zoeza <zahrahabitaa@gmail.com>
2023-03-07 17:05:44 +0100

    Update urls.py

--- src/clients/urls.py
+++ src/clients/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('manage-client/', views.manage_client, name='manage-client'),
     path('add-client/', views.add_client, name='add-client'),
     path('delete-client/<str:sku>', views.delete_client, name='delete-client'),
-    path('edit-client/<int:id>', views.edit_client, name='edit-client'),
+    path('edit-client/<str:id>', views.edit_client, name='edit-client'),
     path('update-client/<int:id>', views.update_client, name='update-client'),
 
 ]
