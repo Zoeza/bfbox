8cfb6f08e75974454e92b66c53f1f5ea1542d91e
Zoeza <zahrahabitaa@gmail.com>
2023-03-07 20:03:18 +0100

    Update views.py

--- src/appointments/views.py
+++ src/appointments/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import Appointment
-from ..clients import views
+import random
+import string
 
 
 # ------------------ add appointment ---------------------- #
@@ -13,7 +14,7 @@ def add_appointment(request):
         title = request.POST.get('title', False)
         client_name = request.POST.get('email', False)
         time = request.POST.get('address', False)
-        sku = views.serial_number_generator(10).upper()
+        sku = serial_number_generator(10).upper()
 
         Appointment(title=title,
                     client_name=client_name,
@@ -42,7 +43,7 @@ def update_appointment(request, id):
         selected_appointment.title = request.POST.get('title', False)
         selected_appointment.client_name = request.POST.get('client_name', False)
         selected_appointment.time = request.POST.get('time', False)
-        selected_appointment.sku = views.serial_number_generator(10).upper()
+        selected_appointment.sku = serial_number_generator(10).upper()
         selected_appointment.save()
         return redirect('appointments:manage-appointment')
 
@@ -62,3 +63,9 @@ def manage_appointment(request):
         'appointments': Appointment.objects.all(),
     }
     return render(request, "appointments/manage_appointment.html", context)
+
+
+def serial_number_generator(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+    return result_str
