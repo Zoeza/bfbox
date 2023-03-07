f785fdbfb57e0f5698077b4aafbb2158c1f3b5a5
Zoeza <zahrahabitaa@gmail.com>
2023-03-07 20:26:46 +0100

    Update views.py

--- src/appointments/views.py
+++ src/appointments/views.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.shortcuts import render
 
 from django.contrib import messages
@@ -15,7 +13,7 @@ def add_appointment(request):
     if request.method == 'POST':
         title = request.POST.get('title', False)
         client_name = request.POST.get('email', False)
-        time = datetime.datetime.strptime(request.POST.get('address', False), '%Y-%m-%d %H:%M:%S')
+        time = request.POST.get('time', False)
         sku = serial_number_generator(10).upper()
 
         Appointment(title=title,
