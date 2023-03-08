bdf05e5329c79469b88d9d54b7c27586a1920acc
Zoeza <zahrahabitaa@gmail.com>
2023-03-08 16:29:46 +0100

    update

--- src/templates_manager/models.py
+++ src/templates_manager/models.py
@@ -6,7 +6,7 @@ class UploadTemplate(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     template = models.FileField(upload_to='file_uploader/', null=True)
     sku = models.CharField(max_length=20, unique=True, null=True)
-    last_modifided = models.DateTimeField(auto_now_add=True, null=True)
+    last_modified = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
