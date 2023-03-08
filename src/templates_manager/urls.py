edfc0251c324df041b5436508513c73873bf7c56
Zoeza <zahrahabitaa@gmail.com>
2023-03-08 19:02:39 +0100

    update

--- src/templates_manager/urls.py
+++ src/templates_manager/urls.py
@@ -6,6 +6,7 @@ app_name = 'templates_manager'
 urlpatterns = [
 
     path('manage-template/', views.manage_template, name='manage-template'),
+    path('upload-template/', views.upload_template, name='upload-template'),
     path('delete-template/<str:sku>', views.delete_template, name='delete-template'),
 
 ]
--- src/templates_manager/views.py
+++ src/templates_manager/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.http import FileResponse, HttpResponse
 from .models import UploadTemplate
+from clients.functions import serial_number_generator
 
 
 # ------------------ manage template -------------------------- #
@@ -12,9 +13,27 @@ def manage_template(request):
         "templates": UploadTemplate.objects.all()
     }
     return render(request, "templates_manager/manage_template.html", context)
+
+
 # ------------------ end manage template ---------------------- #
 
+# ------------------ add client ---------------------- #
 
+def upload_template(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', False)
+        template = request.FILES.get('template', False)
+        modified_at = request.POST.get('birthday', False)
+        sku = serial_number_generator(10).upper()
+        UploadTemplate(name=name,
+                       modified_at=modified_at,
+                       template=template,
+                       sku=sku,
+                       ).save()
+    return redirect('templates_manager:manage-template')
+
+
+# ------------------ end add client ---------------------- #
 # ------------------ delete template -------------------------- #
 
 def delete_template(request, sku):
@@ -22,7 +41,4 @@ def delete_template(request, sku):
 
     return redirect('templates_manager:manage-template')
 
-
 # ------------------ end delete template ---------------------- #
-
-
