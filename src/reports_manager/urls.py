3d1241472d15333c3dee516179cb2e789f73508c
Zoeza <zahrahabitaa@gmail.com>
2023-03-09 22:01:41 +0100

    update

---
+++ src/reports_manager/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'reports_manager'
+
+urlpatterns = [
+
+    path('manage-report/', views.manage_report(), name='manage-report'),
+
+
+]
