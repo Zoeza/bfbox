e76721471ea5c845321a2354b03996f4d337e61e
Zoeza <zahrahabitaa@gmail.com>
2023-03-09 22:15:00 +0100

    update

--- src/bfbox/urls.py
+++ src/bfbox/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
                   path('clients/', include('clients.urls')),
                   path('appointments/', include('appointments.urls')),
                   path('templates_manager/', include('templates_manager.urls')),
-
+                  path('reports_manager/', include('reports_manager.urls')),
                   path('admin/', admin.site.urls),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/reports_manager/views.py
+++ src/reports_manager/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import FileResponse, HttpResponse
-from .models import UploadTemplate
+
 from clients.functions import serial_number_generator
 
 
+def manage_template(request):
+
+    return render(request, "templates_reports/manage_report.html", {})
 
