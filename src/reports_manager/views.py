cd14d821456330c51a7e65f1db33b12684b5bf31
Zoeza <zahrahabitaa@gmail.com>
2023-03-12 00:09:46 +0100

    update

--- src/reports_manager/views.py
+++ src/reports_manager/views.py
@@ -17,7 +17,8 @@ def manage_report(request):
 def add_report(request):
     if request.method == "POST":
         template_selected = request.POST.get('template_selected')
+        context = {
+            "template_selected": template_selected
+        }
 
-        return render(request, "reports_manager/add_report.html", template_selected)
-
-
+        return render(request, "reports_manager/add_report.html", context)
