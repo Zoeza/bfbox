140d8e65f4ddc8b56adcddbbe59a13b34501eda9
Zoeza <zahrahabitaa@gmail.com>
2023-03-11 21:44:21 +0100

    update

--- src/reports_manager/urls.py
+++ src/reports_manager/urls.py
@@ -6,6 +6,6 @@ app_name = 'reports_manager'
 urlpatterns = [
 
     path('manage-report/', views.manage_report, name='manage-report'),
-    path('add-report/<str:sku', views.add_report, name='add-report'),
+    path('add-report/<str:template_name>', views.add_report, name='add-report'),
 
 ]
