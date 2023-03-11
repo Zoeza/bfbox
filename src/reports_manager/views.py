dc5d83010664041df34f530febbb3fa58b38544a
Zoeza <zahrahabitaa@gmail.com>
2023-03-11 22:52:57 +0100

    Update views.py

--- src/reports_manager/views.py
+++ src/reports_manager/views.py
@@ -21,7 +21,7 @@ def add_report(request):
         if template_name == "Notice letter":
             report_actions.add_notice_letter(request)
 
-        return HttpResponse('<h2> template_name.</h2>')
+        return HttpResponse('<h2> {{template_name}}.</h2>')
 
     redirect('reports_manager:manage-report')
 
