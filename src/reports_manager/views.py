5e63dcfc636fd22c38028b5df2b28684d57241a5
Zoeza <zahrahabitaa@gmail.com>
2023-03-11 16:27:22 +0100

    update

--- src/reports_manager/views.py
+++ src/reports_manager/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
+from template_manager.models import UploadTemplate
 from django.http import FileResponse, HttpResponse
 
 from clients.functions import serial_number_generator
 
 
 def manage_report(request):
-    return render(request, "reports_manager/manage_report.html", {})
+    templates = UploadTemplate.objects.all()
+    context = {
+        "templates": templates
+    }
+    return render(request, "reports_manager/manage_report.html", context)
 
 
 def add_report(request):
