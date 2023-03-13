6a57363f9903d8c7f269add1d95842416f151dd8
Zoeza <zahrahabitaa@gmail.com>
2023-03-13 08:00:10 +0100

    update

--- src/reports_manager/report_actions.py
+++ src/reports_manager/report_actions.py
@@ -3,6 +3,6 @@ from templates_manager.models import UploadTemplate
 from docxtpl import DocxTemplate
 
 
-def add_notice_letter(request):
+def add_notice_letter(request, template_name):
     url = "reports_manager/add_report.html"
-    return {'url': url, }
+    return render(request, url, {'template_name': template_name})
