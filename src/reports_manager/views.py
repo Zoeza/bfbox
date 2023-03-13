24e0c7350db0564882c260516b57cbd512accc54
Zoeza <zahrahabitaa@gmail.com>
2023-03-13 19:57:51 +0100

    Update views.py

--- src/reports_manager/views.py
+++ src/reports_manager/views.py
@@ -22,7 +22,7 @@ def manage_report(request):
     if request.method == 'POST':
         template_name = request.POST.get('template_name')
         if template_name == 'Notice letter':
-            url = "report_manager/add_report.html"
+            url = "reports_manager/add_report.html"
 
     context = {
         "reports": GeneratedReport.objects.all(),
