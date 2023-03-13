84180fff71c608915d73534a3bc785945a9f95bd
Zoeza <zahrahabitaa@gmail.com>
2023-03-13 18:26:48 +0100

    Update urls.py

--- src/reports_manager/urls.py
+++ src/reports_manager/urls.py
@@ -6,7 +6,6 @@ app_name = 'reports_manager'
 urlpatterns = [
 
     path('manage-report/', views.manage_report, name='manage-report'),
-    path('add-report/', views.add_report, name='add-report'),
     path('submit-notice-letter/', views.submit_notice_letter, name='submit-notice-letter'),
     path('download-report/<int:id>', views.download_report, name='download-report'),
 
