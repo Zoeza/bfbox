5ec4b92671c9c93221b3764ba0102649c8afa133
Zoeza <zahrahabitaa@gmail.com>
2023-03-13 16:33:55 +0100

    Update report_actions.py

--- src/reports_manager/report_actions.py
+++ src/reports_manager/report_actions.py
@@ -39,10 +39,10 @@ def add_notice_letter(request, template_name):
         # return FileResponse(report_io, as_attachment=True, filename=f'notice_letter.docx')
 
         notice_letter.file.save('notice_letter.docx', ContentFile(report_io.read()))
-        notice_letter.filename = template_name
-        # notice_letter.client = request.POST.get('court_case_applicants')
-        # notice_letter.lawyer = request.POST.get('court_case_lawyer')
-        # notice_letter.save()
+        notice_letter.filename = request.POST.get('court_case_num')
+        notice_letter.client = request.POST.get('court_case_applicants')
+        notice_letter.lawyer = request.POST.get('court_case_lawyer')
+        notice_letter.save()
         messages.success(request, " New Report Generated successfully !!")
 
         return render(request, "reports_manager/add_notice_letter.html", {'sku': notice_letter.id})
