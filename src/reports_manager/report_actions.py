d96c7a82a55b2358e9acabc2b90932345737af51
Zoeza <zahrahabitaa@gmail.com>
2023-03-13 08:13:22 +0100

    Update report_actions.py

--- src/reports_manager/report_actions.py
+++ src/reports_manager/report_actions.py
@@ -4,5 +4,5 @@ from docxtpl import DocxTemplate
 
 
 def add_notice_letter(request, template_name):
-    url = "reports_manager/add_report.html"
-    return render(request, url, {'template_name': template_name})
+
+    return render(request, "reports_manager/add_report.html", {'template_name': template_name})
