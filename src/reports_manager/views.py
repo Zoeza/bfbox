4b7a3a0b18f79f4ddf18e20d7083ef380d3e30d4
Zoeza <zahrahabitaa@gmail.com>
2023-03-13 14:57:38 +0100

    Update views.py

--- src/reports_manager/views.py
+++ src/reports_manager/views.py
@@ -34,21 +34,21 @@ def add_report(request):
     if request.method == 'POST':
         template = UploadTemplate.objects.get(name="Notice Letter")
         template_path = template.template.path
+
         report = DocxTemplate(template_path)
         notice_letter = GeneratedReport()
-
         context = {
-            'court_case_applicants': request.POST.get('court_case_applicants'),
-            'bailiff_name': request.POST.get('bailiff_name'),
-            'court_case_num': request.POST.get('court_case_num'),
-            'bailiff_address': request.POST.get('bailiff_address'),
-            'court_case_date': request.POST.get('court_case_date'),
-            'court_case_time': request.POST.get('court_case_time'),
-            'court_case_msg_title': request.POST.get('court_case_msg_title'),
-            'court_case_lawyer': request.POST.get('court_case_lawyer'),
-            'court_case_agent': request.POST.get('court_case_agent'),
-            'court_case_defendants': request.POST.get('court_case_defendants'),
-            'court_case_msg_content': request.POST.get('court_case_msg_content'),
+            'court_case_applicants': request.POST['court_case_applicants'],
+            'bailiff_name': request.POST['bailiff_name'],
+            'court_case_num': request.POST['court_case_num'],
+            'bailiff_address': request.POST['bailiff_address'],
+            'court_case_date': request.POST['court_case_date'],
+            'court_case_time': request.POST['court_case_time'],
+            'court_case_msg_title': request.POST['court_case_msg_title'],
+            'court_case_lawyer': request.POST['court_case_lawyer'],
+            'court_case_agent': request.POST['court_case_agent'],
+            'court_case_defendants': request.POST['court_case_defendants'],
+            'court_case_msg_content': request.POST['court_case_msg_content'],
 
         }
 
@@ -56,18 +56,17 @@ def add_report(request):
         report_io = io.BytesIO()  # create a file-like object
         report.save(report_io)  # save data to file-like object
         report_io.seek(0)  # go to the beginning of the file-like object
-        return FileResponse(report_io, as_attachment=True, filename=f'notice_letter.docx')
+        # return FileResponse(report_io, as_attachment=True, filename=f'notice_letter.docx')
 
         notice_letter.file.save('notice_letter.docx', ContentFile(report_io.read()))
-        notice_letter.filename = request.POST.get('court_case_num')
-        notice_letter.client = request.POST.get('court_case_applicants')
-        notice_letter.lawyer = request.POST.get('court_case_lawyer')
+        notice_letter.filename = request.POST['court_case_num']
+        notice_letter.client = request.POST['court_case_applicants']
+        notice_letter.lawyer = request.POST['court_case_lawyer']
         notice_letter.save()
         messages.success(request, " New Report Generated successfully !!")
+        return render(request, "reports_manager/add_report.html", {'sku': notice_letter.id})
 
-        # return render(request, "reports_manager/add_report.html", {'sku': notice_letter.id})
-
-    return render(request, "reports_manager/add_report.html", {})
+    return redirect('reports-manager:add-report')
 
 
 def download_report(request, id):
