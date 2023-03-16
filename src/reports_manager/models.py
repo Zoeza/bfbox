e2138764a204cdac9a221c7f1ba1a700f8f9033d
Zoeza <zahrahabitaa@gmail.com>
2023-03-16 23:26:27 +0100

    update

--- src/reports_manager/models.py
+++ src/reports_manager/models.py
@@ -8,7 +8,7 @@ class GeneratedReport(models.Model):
     lawyer = models.CharField(max_length=255)
     sku = models.CharField(max_length=20, unique=True, null=True)
     number = models.PositiveIntegerField(null=True, blank=True)
-    file = models.FileField(upload_to='', null=True)
+    file = models.FileField(upload_to='reports/', null=True)
     last_modified = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
