257a95db6e019f88e45db2af5ead2c8776acf9ba
Zoeza <zahrahabitaa@gmail.com>
2023-03-16 23:16:22 +0100

    update

--- src/reports_manager/models.py
+++ src/reports_manager/models.py
@@ -7,7 +7,8 @@ class GeneratedReport(models.Model):
     client = models.CharField(max_length=255)
     lawyer = models.CharField(max_length=255)
     sku = models.CharField(max_length=20, unique=True, null=True)
-    file = models.FileField(upload_to='file/', null=True)
+    number = models.PositiveIntegerField()
+    file = models.FileField(upload_to='', null=True)
     last_modified = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
