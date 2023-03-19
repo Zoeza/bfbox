c4de992367c4df129dff7d49a41f90c5e22b2627
Zoeza <zahrahabitaa@gmail.com>
2023-03-19 16:29:00 +0100

    Update views.py

--- src/reports_manager/views.py
+++ src/reports_manager/views.py
@@ -36,7 +36,7 @@ def manage_report(request, action, sku):
 
 def add_report(request):
     if request.method == 'POST':
-        template = request.POST.get('template.name')
+        template = request.POST.get('{{template.name}}')
         if template == 'Notice letter':
             return "reports_manager/test.html"
 
