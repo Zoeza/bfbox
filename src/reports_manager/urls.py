ecd1fa519ab259e5d24b91ae6436364b9bb4235f
Zoeza <zahrahabitaa@gmail.com>
2023-03-19 19:48:31 +0100

    update

--- src/reports_manager/urls.py
+++ src/reports_manager/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 
     path('<str:action>/<str:sku>/manage-report/', views.manage_report, name='manage-report'),
-    path('<str:action>/add-report/', views.add_report, name='add-report'),
+    path('<str:action>/<str:template_name>/add-report/', views.add_report, name='add-report'),
 
 ]
