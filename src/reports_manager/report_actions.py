eb3d87ef9546d786e9e43ad177f0cda45f0d873c
Zoeza <zahrahabitaa@gmail.com>
2023-03-19 01:55:49 +0100

    Update report_actions.py

--- src/reports_manager/report_actions.py
+++ src/reports_manager/report_actions.py
@@ -46,10 +46,9 @@ def generate_report(template_name, context):
 
 def save_report(filename, court_case_num, file):
     sku = serial_number_generator(10).upper()
-
+    GeneratedReport.file.save('filename.docx', file)
     GeneratedReport(filename=filename,
                     number=court_case_num,
-                    file=file,
                     sku=sku,
                     ).save()
 
