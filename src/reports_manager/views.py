4ab2d137d2db163106072a8279e935fc8ff53ae8
Zoeza <zahrahabitaa@gmail.com>
2023-03-19 19:37:07 +0100

    Update views.py

--- src/reports_manager/views.py
+++ src/reports_manager/views.py
@@ -42,6 +42,6 @@ def manage_report(request, action, sku):
 def add_report(request, action):
     url = "reports_manager/add_report.html"
     if action == 'add_notice_letter':
-        url = report_actions.add_notice_letter(request).get('url')
+        report_actions.add_notice_letter(request)
 
     return render(request, url, {})
