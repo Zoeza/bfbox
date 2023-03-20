6fce9ace5b93a0581d419c5ee0fb64045484a984
Zoeza <zahrahabitaa@gmail.com>
2023-03-20 17:53:46 +0100

    Update views.py

--- src/reports_manager/views.py
+++ src/reports_manager/views.py
@@ -54,4 +54,4 @@ def choose_report(request):
 def generate_report(request, action):
     if action == 'generate_notice_letter':
         report_actions.generate_notice_letter(request)
-    return redirect('dashboard')
+    return redirect('choose-report')
