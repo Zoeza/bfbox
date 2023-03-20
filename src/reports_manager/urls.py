36070675cb2dc858a341d13ed2bc5804f186b2c7
Zoeza <zahrahabitaa@gmail.com>
2023-03-20 17:08:45 +0100

    update

--- src/reports_manager/urls.py
+++ src/reports_manager/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-
+app_name = 'manage_reports'
 urlpatterns = [
 
-    path('<str:action>/<str:sku>/manage-report/', views.manage_report, name="manage-report"),
-    path('<str:action>/generate-report/', views.generate_report, name="generate-report"),
-    path('choose-report/', views.choose_report, name="choose-report"),
+    path('<str:action>/<str:sku>/manage-report/', views.manage_report, name='manage-report'),
+    path('<str:action>/generate-report/', views.generate_report, name='generate-report'),
+    path('choose-report/', views.choose_report, name='choose-report'),
 
 ]
