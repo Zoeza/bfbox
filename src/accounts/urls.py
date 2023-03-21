7de990f820f5a1687520370175e98a74e1ef1bc5
Zoeza <zahrahabitaa@gmail.com>
2023-03-21 20:26:34 +0100

    update

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 
     path('sign-up/', views.sign_up, name='sign-up'),
+    path('sign-in/', views.sign_in, name='sign-in'),
 
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def sign_up(request):
     return render(request, "accounts/sign_up.html", {})
+
+
+def sign_in(request):
+    return render(request, "accounts/sign_in.html", {})
