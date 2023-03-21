cd7dfe440c2bf3d4e2fa11693b9d7d56dc8ed973
Zoeza <zahrahabitaa@gmail.com>
2023-03-21 21:32:47 +0100

    update

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from reports_manager.models import GeneratedReport
+from clients.models import Client
+from appointments.models import Appointment
 
 
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html", {})
+    reports_list = GeneratedReport.objects.all()
+    clients_list = Client.objects.all()
+    appointments_list = Appointment.objects.all()
+    context = {
+        "reports_list": reports_list,
+        "clients_list": clients_list,
+        "appointments_list": appointments_list,
+    }
+    return render(request, "dashboard/dashboard.html", context)
