eb261cd1d387ad3a21747c03a1affcc9df642256
Zoeza <zahrahabitaa@gmail.com>
2023-03-22 15:15:38 +0100

    Update urls.py

--- src/templates_manager/urls.py
+++ src/templates_manager/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('upload-template/', views.upload_template, name='upload-template'),
     path('delete-template/<str:sku>', views.delete_template, name='delete-template'),
     path('edit-template/<str:sku>', views.edit_template, name='edit-template'),
-    path('download-template/<str:sku>', views.download_template, name='download--template'),
+    path('download-template/<str:sku>', views.download_template, name='download-template'),
 
 ]
