faf355a7d14e025d6344c977eaae33d1d3a48750
Zoeza <zahrahabitaa@gmail.com>
2023-03-24 06:36:07 +0100

    update

--- src/bfbox/urls.py
+++ src/bfbox/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
                   path('reports_manager/', include('reports_manager.urls')),
                   path('accounts/', include('accounts.urls')),
 
-                  #  path('admin/', admin.site.urls),
+                    path('admin/', admin.site.urls),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
