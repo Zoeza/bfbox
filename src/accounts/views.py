32fbee034f56f63e5b479c8d6e52f0a60896be34
Zoeza <zahrahabitaa@gmail.com>
2023-03-29 23:11:25 +0100

    update

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .models import user_type, User
 
 
 # Create your views here.
 def sign_up(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first-name')
+        last_name = request.POST.get('last-name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        bailiff = request.POST.get('is_bailiff')
+        employee = request.POST.get('is_employee')
+
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+
+        usertype = None
+        if bailiff:
+            usertype = user_type(user=user, is_bailiff=True)
+        elif employee:
+            usertype = user_type(user=user, is_employee=True)
+
+        usertype.save()
+        # Successfully registered. Redirect to homepage
+        return redirect('sign-in')
     return render(request, "accounts/register.html", {})
 
 
