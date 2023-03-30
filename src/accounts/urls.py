60c6028855366e8260a0532509e37a632900ca3e
Zoeza <zahrahabitaa@gmail.com>
2023-03-30 13:27:50 +0100

    update

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 
     path('sign-up/', views.sign_up, name='sign-up'),
     path('sign-in/', views.sign_in, name='sign-in'),
+    path('sign-out/', views.sign_out, name='sign-out'),
 
 ]
