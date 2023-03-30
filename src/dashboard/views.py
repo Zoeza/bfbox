8c5a8bb9c626fb80ae80411cc09604190cb5578d
Zoeza <zahrahabitaa@gmail.com>
2023-03-30 14:34:01 +0100

    update

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -6,19 +6,16 @@ from accounts.models import user_type
 from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def dashboard(request):
     reports_list = GeneratedReport.objects.all()
     clients_list = Client.objects.all()
     appointments_list = Appointment.objects.all()
-    usertype = 'Employee'
-    if user_type.is_bailiff:
-        usertype = 'Bailiff'
 
     context = {
         "reports_list": reports_list,
         "clients_list": clients_list,
         "appointments_list": appointments_list,
-        "user_type": usertype,
 
     }
 
