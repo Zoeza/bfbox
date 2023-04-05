fd8248aa3b7d84d990d523f678c7eb2dd800f003
Zoeza <zahrahabitaa@gmail.com>
2023-04-05 01:56:44 +0100

    Update report_actions.py

--- src/reports_manager/report_actions.py
+++ src/reports_manager/report_actions.py
@@ -78,7 +78,7 @@ def download_report(sku):
 
 def docx_to_pdf(sku):
     report_selected = GeneratedReport.objects.get(sku=sku)
-    convert(report_selected.file, report_selected.pdf + "word_images.pdf")
+    convert(report_selected.file, report_selected.pdf)
 
     # report_selected.pdf.save('Notice_letter.pdf', convert(report_selected.file))
     # pdf = (report_selected.pdf.read(), 'r')
