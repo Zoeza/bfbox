f3ac512ea3c555099d5c193c03e16002fff2a44f
Zoeza <zahrahabitaa@gmail.com>
2023-04-06 15:25:33 +0100

    update

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .models import user_type, User
@@ -5,6 +6,9 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+# ------------ sign up page -------------#
+
+@login_required
 def sign_up(request):
     if not request.session.get('language', None):
         request.session['language'] = 'ar'
@@ -37,6 +41,8 @@ def sign_up(request):
     return render(request, url, {})
 
 
+# ------------ sign in page -------------#
+
 def sign_in(request):
     if not request.session.get('language', None):
         request.session['language'] = 'ar'
@@ -55,7 +61,30 @@ def sign_in(request):
         return render(request, url, {})
 
 
+# ------------ sign out page -------------#
+
 @login_required
 def sign_out(request):
     logout(request)
     return redirect('sign-in')
+
+
+# ------------ user management -------------#
+@login_required
+def manage_user(request, action, sku):
+    direction = request.session.get('language')
+    try:
+        users_list = User.objects.all()
+        usertype_list = user_type.objects.all()
+    except User.DoesNotExist:
+        raise Http404("No users")
+
+    url = direction + "/accounts/manage_user.html"
+
+    context = {
+        "users_list": users_list,
+        "usertype_list": usertype_list,
+
+    }
+    return render(request, url, context)
+
