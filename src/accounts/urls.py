2fc0e23888b1eb34ec647fbf3bf721f64f42f795
Zoeza <zahrahabitaa@gmail.com>
2023-04-07 23:33:53 +0100

    update

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('sign-up/', views.sign_up, name='sign-up'),
     path('sign-in/', views.sign_in, name='sign-in'),
     path('sign-out/', views.sign_out, name='sign-out'),
-    path('<str:action>/<email>/manage-user/', views.manage_user, name='manage-user'),
+    path('<str:action>/<first_name>/manage-user/', views.manage_user, name='manage-user'),
 
 ]
