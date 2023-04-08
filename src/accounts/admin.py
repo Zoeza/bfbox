cfbbb4bdf099d846b9fcf0e640d600e8af453470
Zoeza <zahrahabitaa@gmail.com>
2023-04-08 01:03:19 +0100

    update

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -7,7 +7,7 @@ from .models import User, user_type
 
 class UserAdmin(BaseUserAdmin):
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'first_name', 'last_name', 'last_login')}),
+        (None, {'fields': ('email', 'password', 'last_login')}),
         ('Permissions', {'fields': (
             'is_active',
             'is_staff',
@@ -21,12 +21,12 @@ class UserAdmin(BaseUserAdmin):
             None,
             {
                 'classes': ('wide',),
-                'fields': ('first_name', 'last_name', 'email', 'password1', 'password2')
+                'fields': ( 'email', 'password1', 'password2')
             }
         ),
     )
 
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'last_login')
+    list_display = ('email', 'is_staff', 'last_login')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('email',)
     ordering = ('email',)
