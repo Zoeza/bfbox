c92bda5ff82990b646047c6a57623feeec4a8163
Zoeza <zahrahabitaa@gmail.com>
2023-04-08 01:28:31 +0100

    Update admin.py

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -26,7 +26,7 @@ class UserAdmin(BaseUserAdmin):
         ),
     )
 
-    list_display = ('last_name', 'is_staff', 'last_login')
+    list_display = ('email', 'is_staff', 'last_login')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('email',)
     ordering = ('email',)
